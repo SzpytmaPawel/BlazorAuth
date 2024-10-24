872df10ec9eb64fe695500b3e07ad36ea78c0bfa
Pawel Szpytma <pawel.szpytma@opusdatum.com>
2024-10-24 12:58:02 +0000

    initial innit

---
+++ BlazorAuth/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAuth
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private Task<AuthenticationState> _authenticationStateTask = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+
+        public override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            return _authenticationStateTask;
+        }
+
+        public Task SetAuthenticationState(Task<AuthenticationState> authenticationStateTask)
+        {
+            _authenticationStateTask = authenticationStateTask;
+            NotifyAuthenticationStateChanged(_authenticationStateTask);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BlazorAuth/Program.cs
@@ -0,0 +1,49 @@
+//Program.cs
+using BlazorAuth.Components;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace BlazorAuth
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+
+            builder.Services.AddAuthentication();
+            builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                });
+
+            var app = builder.Build();
+
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+
+            app.UseAntiforgery();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
+
