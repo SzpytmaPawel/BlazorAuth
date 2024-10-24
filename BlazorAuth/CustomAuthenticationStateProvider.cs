24605f3a4d794fa73b89459a7f49ba287cfea0a4
Pawel Szpytma <pawel.szpytma@opusdatum.com>
2024-10-24 14:23:32 +0000

    multitab authimple,emtned - todo fix login after logout

--- BlazorAuth/CustomAuthenticationStateProvider.cs
+++ BlazorAuth/CustomAuthenticationStateProvider.cs
@@ -1,22 +1,36 @@
-﻿using Microsoft.AspNetCore.Components.Authorization;
+﻿// CustomAuthenticationStateProvider.cs
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace BlazorAuth
 {
     public class CustomAuthenticationStateProvider : AuthenticationStateProvider
     {
-        private Task<AuthenticationState> _authenticationStateTask = Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+        private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+
+        private ClaimsPrincipal _currentUser;
 
         public override Task<AuthenticationState> GetAuthenticationStateAsync()
         {
-            return _authenticationStateTask;
+            return Task.FromResult(new AuthenticationState(_currentUser ?? _anonymous));
         }
 
-        public Task SetAuthenticationState(Task<AuthenticationState> authenticationStateTask)
+        public void MarkUserAsAuthenticated(string userName)
         {
-            _authenticationStateTask = authenticationStateTask;
-            NotifyAuthenticationStateChanged(_authenticationStateTask);
-            return Task.CompletedTask;
+            var authenticatedUser = new ClaimsPrincipal(new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, userName)
+            }, "apiauth"));
+
+            _currentUser = authenticatedUser;
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(authenticatedUser)));
+        }
+
+        public void MarkUserAsLoggedOut()
+        {
+            _currentUser = _anonymous;
+            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(_anonymous)));
         }
     }
 }
--- BlazorAuth/Program.cs
+++ BlazorAuth/Program.cs
@@ -1,6 +1,9 @@
-//Program.cs
+// Program.cs
 using BlazorAuth.Components;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 
 namespace BlazorAuth
 {
@@ -14,13 +17,13 @@ namespace BlazorAuth
             builder.Services.AddRazorComponents()
                 .AddInteractiveServerComponents();
 
-
-            builder.Services.AddAuthentication();
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                 {
-                    options.LoginPath = "/login";
+                    options.LoginPath = "/login"; 
                 });
+
+            builder.Services.AddAuthorization();
 
             var app = builder.Build();
 
@@ -34,10 +37,38 @@ namespace BlazorAuth
 
             app.UseStaticFiles();
 
-
-            app.UseAntiforgery();
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseAntiforgery();
+
+            app.MapPost("/process-login", async context =>
+            {
+                var form = await context.Request.ReadFormAsync();
+                string username = form["username"];
+                string password = form["password"];
+                string returnUrl = form["ReturnUrl"].FirstOrDefault() ?? "/";
+
+                if (username == "user" && password == "password")
+                {
+                    var claims = new[] { new Claim(ClaimTypes.Name, username) };
+                    var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                    var principal = new ClaimsPrincipal(identity);
+
+                    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+                    context.Response.Redirect(returnUrl);
+                }
+                else
+                {
+                    context.Response.Redirect($"/login?error=Invalid+username+or+password&ReturnUrl={System.Net.WebUtility.UrlEncode(returnUrl)}");
+                }
+            });
+
+            app.MapGet("/logout", async context =>
+            {
+                await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+                context.Response.Redirect("/login");
+            });
 
             app.MapRazorComponents<App>()
                 .AddInteractiveServerRenderMode();
@@ -46,4 +77,3 @@ namespace BlazorAuth
         }
     }
 }
-
